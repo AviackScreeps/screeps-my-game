9baa794aa84248956a8be6d3c89d4d4ea3eda81c
Aviak <aviack@mail.ru>
2019-07-31 20:59:16 +0300

    h

--- role.harvester.js
+++ role.harvester.js
@@ -11,15 +11,15 @@ var roleHarvester = {
             }
         }
         else {
-            var targets = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+            var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
                 filter: (structure) => {
                     return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
                         structure.energy < structure.energyCapacity;
                 }
             });
-            if (targets.length > 0) {
-                if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], { visualizePathStyle: { stroke: '#ffffff' } });
+            if (target != undefined) {
+                if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target, { visualizePathStyle: { stroke: '#ffffff' } });
                 }
             } else {
                 creep.moveTo(Game.flags.IdleFlag);
