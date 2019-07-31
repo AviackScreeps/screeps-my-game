1f26a098e353c8740f848e627175bb3848844b66
Aviak <aviack@mail.ru>
2019-07-31 23:28:29 +0300

    let builders repair all ramparts

--- role.builder.js
+++ role.builder.js
@@ -13,10 +13,14 @@ var roleBuilder = {
         else {
             if (creep.carry.energy == 0) {
                 creep.memory.working = false;
+                if (creep.memory.target != undefined) {
+                    delete creep.memory.undefined;
+                }
                 return;
 
             }
             creep.memory.working = true;
+
             var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
                 filter: function (object) {
                     return ((object.hits / object.hitsMax) < 0.95 && object.structureType != STRUCTURE_WALL && object.structureType != STRUCTURE_RAMPART);
@@ -38,15 +42,29 @@ var roleBuilder = {
                     }
                 } else {
                     //console.log(creep + " is walling");
-                    var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-                        filter: (s) =>  s.structureType == STRUCTURE_RAMPART 
-                    });
+
+                    target = creep.memory.target;
+                    if (target == undefined) {
+                        var targets = creep.room.find(FIND_STRUCTURES, {
+                            filter: (s) => s.structureType == STRUCTURE_RAMPART
+                        });
+
+                        var minhp = 1000000;
+                        for (let structure of targets) {
+                            if (structure.hits < minhp) {
+                                minhp = structure.hits;
+                                target = structure;
+                            }
+                        }
+                    }
+
              
                     repairResult = creep.repair(target);
                     if (target == undefined) {
                         //console.log('no rampart to repair');
                     } else {
                         //console.log("repairing " + target.structureType + " " + target + " " + repairResult);
+                        creep.memory.target = target;
                         if (repairResult == ERR_NOT_IN_RANGE) {
                             creep.moveTo(target, { visualizePathStyle: { stroke: '#ffffff' } });
                         }
