c5c6eb3e0fd737d7d9df83d99ed22f574fe50953
Aviak <aviack@mail.ru>
2019-08-01 21:03:49 +0300

    1

--- role.supplyUpgrader.js
+++ role.supplyUpgrader.js
@@ -43,8 +43,9 @@ var roleSupplyUpgrader = {
             } else {
                 creep.memory.transfering = true;
                 if (Game.spawns['Spawn1'].energy < Game.spawns['Spawn1'].energyCapacity) {
-                    creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    if (creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                         creep.moveTo(Game.spawns['Spawn1']);
+                    }
                 } else {
                     if (creep.transfer(creep.room.controller, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                         creep.moveTo(creep.room.controller);
