89e95e479ab444ec2268f5c917b585a15c5ba369
Aviak <aviack@mail.ru>
2019-08-02 21:55:11 +0300

    get upgraders use containers

--- role.upgrader.js
+++ role.upgrader.js
@@ -5,10 +5,52 @@ var roleUpgrader = {
     /** @param {Creep} creep **/
     run: function (creep) {
         if (creep.carry.energy < creep.carryCapacity && creep.memory.transfering == false) {
-            var source = creep.pos.findClosestByPath(FIND_SOURCES);
-            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source, { visualizePathStyle: { stroke: '#bdbdbd' } });
+
+            var targetContainer = undefined;
+            if (creep.memory.targetContainerId == undefined) {
+
+                var containers = creep.room.find(FIND_STRUCTURES, { filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] > 500 })
+
+                var maxContainer = undefined;
+                var maxEnergy = 0;
+                for (let cont of containers) {
+                    if (maxEnergy < cont.store[RESOURCE_ENERGY]) {
+                        maxEnergy = cont.store[RESOURCE_ENERGY];
+                        maxContainer = cont;
+                    }
+                }
+
+                if (maxContainer != undefined) {
+                    creep.memory.targetContainerId = maxContainer.id;
+                }
+                targetContainer = maxContainer;
             }
+            else {
+                targetContainer = Game.getObjectById(creep.memory.targetContainerId);
+            }
+
+            if (targetContainer != undefined) {
+                var result = creep.withdraw(targetContainer, RESOURCE_ENERGY);
+                //console.log(result);
+                if (result == ERR_NOT_IN_RANGE) {
+
+                    if (creep.memory.pathToContainer == undefined) {
+                        creep.memory.pathToContainer = creep.pos.findPathTo(targetContainer.pos, { ignoreCreeps: true })
+                    }
+                    creep.moveByPath(creep.memory.pathToContainer);
+                }
+                else {
+                    creep.memory.targetContainerId = undefined;
+                    creep.memory.pathToContainer = undefined;
+                }
+            } else {
+                var source = creep.pos.findClosestByPath(FIND_SOURCES);
+                if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(source);
+                }
+            }
+
+            
         }
         else {
             if (creep.carry.energy == 0) {
