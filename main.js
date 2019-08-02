20598e419db5dbdf384976ce84712eea6f7709e6
Aviak <aviack@mail.ru>
2019-08-02 22:17:42 +0300

    never defend by creeps. tower is ok

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ module.exports.loop = function () {
 
     var underAttack = (hostileCreeps.length > 0);
     //console.log("" + hostileCreeps + " " + underAttack);
-    //underAttack = false;
+    underAttack = false;
 
     if (underAttack) {
         console.log("attacked");
@@ -271,7 +271,7 @@ module.exports.loop = function () {
     }
 
     if (Memory.attackByTower == true) {
-        console.log('towers attacking anyway');
+        //console.log('towers attacking anyway');
         var towers = _.filter(Game.structures, (s) => s.structureType == STRUCTURE_TOWER);
 
         for (let tower of towers) {
